d8d2292e38a061ff61c509ebba764ef5c4c3d278
MVolodya <medvid.volodya@gmail.com>
2018-09-24 20:50:09 +0300

    added apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,33 @@
+import { ApolloServer, gql } from 'apollo-server-koa';
 import Koa from 'koa';
 import morgan from 'koa-morgan';
 import Router from 'koa-router';
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
 const app = new Koa();
 const router = new Router();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+app.use(morgan('dev')).use(router.routes());
+
+// https://github.com/apollographql/apollo-server/issues/1393
+// tslint:disable-next-line
+server.applyMiddleware({ app: app as any });
 
 router.get('/', async ctx => {
   ctx.body = 'Hello World!';
 });
-
-app.use(morgan('dev')).use(router.routes());
 
 app.listen(3000);
 
